256396ececde4cb16bfa09f35a03ac21c7f894a2
Yoshikawa Naohiro <yoshikawa@oniongames.jp>
2017-06-27 10:05:00 +0900

    敵の行動パターンを調整

--- Assets/Scripts/Game/Character/EnemyAi/EnemyStrategy1.cs
+++ Assets/Scripts/Game/Character/EnemyAi/EnemyStrategy1.cs
@@ -5,6 +5,9 @@ using UniRx;
 
 public class EnemyStrategy1 : EnemyStrategy
 {
+    private static readonly float ActionDelay = 0.6f;
+    private static readonly int BulletLength = 5;
+
     private Vector3 initialPosition = new Vector3(0, 200, 0);
     private float scale;
 
@@ -20,26 +23,43 @@ public class EnemyStrategy1 : EnemyStrategy
 
         while (!isDefeated)
         {
-			yield return new WaitForSeconds(1);
+            yield return WaitRandomly(ActionDelay);
 			Owner.Move(initialPosition + new Vector3(0, -10, 0), 10);
 			yield return new WaitForSeconds(0.2f);
             Shot(Owner.LeftHand.transform.localPosition);
 
-			yield return new WaitForSeconds(1);
+			yield return WaitRandomly(ActionDelay);
 			Owner.Move(initialPosition + new Vector3(0, 10, 0), 11);
 
-			yield return new WaitForSeconds(1);
+			yield return WaitRandomly(ActionDelay);
 			Owner.Move(initialPosition + new Vector3(0, -10, 0), 10);
 			yield return new WaitForSeconds(0.2f);
             Shot(Owner.RightHand.transform.localPosition);
 
-			yield return new WaitForSeconds(1);
+			yield return WaitRandomly(ActionDelay);
 			Owner.Move(initialPosition + new Vector3(0, 10, 0), 11);
 		}
     }
 
+    private WaitForSeconds WaitRandomly(float pivotWait)
+    {
+        var delta = UnityEngine.Random.value * 0.5f - 0.25f;
+        return new WaitForSeconds(pivotWait + delta);
+    }
+
     private void Shot(Vector3 localPosition)
     {
-        Owner.Shot(localPosition * scale, 180, 100);
+        for (int i = 0; i < BulletLength; i++)
+        {
+            for (int j = 0; j < BulletLength; j++)
+			{
+                var offset = new Vector3(i - BulletLength / 2, j - BulletLength / 2, 0);
+                var offsetRandom = UnityEngine.Random.insideUnitCircle;
+                var position = localPosition * scale
+                    + offset * scale * 3
+                    + offsetRandom.ToVector3() * scale;
+				Owner.Shot(position, 180, 100);
+            }
+        }
     }
 }
